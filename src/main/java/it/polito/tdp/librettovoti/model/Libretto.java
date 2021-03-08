e769ee6e264c65c2ddc6e87bd6a1384241ac6a89
Fulvio Corno <fulvio.corno@polito.it>
2021-03-08 11:48:48 +0100

    Soluzione punto 2

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -15,6 +15,42 @@ public class Libretto {
 		this.voti.add(v) ;
 	}
 	
+	/* blahhh...
+	
+	public void stampaVotiUguali(int punteggio) {
+		// il Libretto stampa da solo i voti
+		System.out.println();
+	}
+	
+	public String votiUguali(int punteggio) {
+		// calcola una stringa che contiene i voti
+		// sarà poi il chiamante a stamparli
+		// -> Solo il NOME?
+		// -> Tutto il voto.toString()
+	}
+	*/
+	
+	public List<Voto> listaVotiUguali(int punteggio) {
+		List<Voto> risultato = new ArrayList<>() ;
+		for(Voto v: this.voti) {
+			if(v.getVoto()==punteggio) {
+				risultato.add(v) ;
+			}
+		}
+		return risultato ;
+	}
+	
+	public Libretto votiUguali(int punteggio) {
+		Libretto risultato = new Libretto();
+		for(Voto v: this.voti) {
+			if(v.getVoto()==punteggio) {
+				risultato.add(v) ;
+//				risultato.voti.add(v) ;
+			}
+		}
+		return risultato ;
+	}
+		
 	public String toString() {
 		String s = "" ;
 		for(Voto v: this.voti) {
--- src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
@@ -1,6 +1,7 @@
 package it.polito.tdp.librettovoti.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class TestLibretto {
 
@@ -12,10 +13,18 @@ public class TestLibretto {
 		Voto voto1 = new Voto("Analisi 1", 30, LocalDate.of(2019, 2, 15));
 
 		libretto.add(voto1);
+		libretto.add(new Voto("Chimica", 25, LocalDate.of(2019, 9, 15)));
+
 		libretto.add(new Voto("Fisica 1", 28, LocalDate.of(2019, 7, 15)));
 		libretto.add(new Voto("Informatica", 24, LocalDate.of(2019, 9, 15)));
 		
 		System.out.println(libretto) ;
+		
+		List<Voto> venticinque = libretto.listaVotiUguali(25) ;
+		System.out.println(venticinque);
+		
+		Libretto librettoventicinque = libretto.votiUguali(25) ;
+		System.out.println(librettoventicinque) ;
 	}
 
 }
