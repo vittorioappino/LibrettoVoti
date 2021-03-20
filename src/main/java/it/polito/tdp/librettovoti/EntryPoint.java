162ec73fc9baa3eca09d808d50c58fcc164f08cc
Vittorio <Vittorio@LAPTOP-EKSQIIDO.fritz.box>
2021-03-20 18:35:55 +0100

    Trasformazione in MVC (primi passi)

--- src/main/java/it/polito/tdp/librettovoti/EntryPoint.java
+++ src/main/java/it/polito/tdp/librettovoti/EntryPoint.java
@@ -2,6 +2,8 @@ package it.polito.tdp.librettovoti;
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+
+import it.polito.tdp.librettovoti.model.Libretto;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -12,7 +14,12 @@ public class EntryPoint extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Scene.fxml"));
+        Parent root = loader.load();
+        FXMLController controller = loader.getController();
+        
+        Libretto model = new Libretto();
+        controller.setModel(model);
         
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
--- src/main/java/it/polito/tdp/librettovoti/FXMLController.java
+++ src/main/java/it/polito/tdp/librettovoti/FXMLController.java
@@ -1,25 +1,70 @@
 package it.polito.tdp.librettovoti;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.librettovoti.model.Libretto;
+import it.polito.tdp.librettovoti.model.Voto;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-public class FXMLController implements Initializable {
-    
+public class FXMLController {
+	
+	private Libretto model;
+
     @FXML
-    private Label label;
-    
+    private ResourceBundle resources;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private URL location;
+
+    @FXML
+    private TextField txtEsame;
+
+    @FXML
+    private TextField txtVoto;
+
+    @FXML
+    private TextField txtData;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    void handleInserisci(ActionEvent event) {
+    	//Leggi e controlla i dati
+    	String nomeEsame = txtEsame.getText();
+    	if(nomeEsame.length()==0) {
+    		txtResult.setText("ERRORE: nome esame vuoto");
+    		return;
+    	}
+    	String votoEsame = txtVoto.getText();
+    	int votoInt = Integer.parseInt(votoEsame);
+    	String dataEsame = txtData.getText();
+    	LocalDate data = LocalDate.parse(dataEsame);
+    	//TODO: aggiungere tutti i controlli
+    	
+    	//Esegui l'azione 
+    	Voto voto = new Voto(nomeEsame, votoInt, data);
+    	model.add(voto);
+    	
+    	// Aggiorna i risultati (nella View)
+    	txtResult.setText(model.toString());
+    }
+
+    @FXML
+    void initialize() {
+        assert txtEsame != null : "fx:id=\"txtEsame\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtVoto != null : "fx:id=\"txtVoto\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtData != null : "fx:id=\"txtData\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+
     }
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    public void setModel(Libretto model) {
+    	this.model=model;
+    }
 }
